6dd78e1f84c48110c101ac5ca7f88f0c9e6aaf6d
최영훈 <c_yh0327@naver.com>
2020-07-23 23:26:33 +0900

    sequelize

---
+++ Javascript/null_undefined.js
@@ -0,0 +1,18 @@
+console.log('foo: ' + foo);
+console.log('typeof foo :',typeof foo);
+var foo = null;
+console.log('typeof foo :',typeof foo);
+
+console.log('undefined null');
+console.log('typeof null : ' + typeof null);
+console.log('typeof undefined : ' + typeof undefined);
+console.log('undefined === null  :'+ (undefined===null));
+console.log('undefined == null  :'+ (undefined==null));
+console.log('undefined === null  :'+ (undefined));
+console.log('undefined :'+ (undefined));
+console.log('null :'+ (null));
+console.log('!undefined :'+ !(undefined));
+console.log('!null :'+ !(null));
+
+var bar;
+
